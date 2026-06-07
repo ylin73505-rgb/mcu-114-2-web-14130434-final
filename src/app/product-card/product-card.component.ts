4e282c3541a29cadfcd177772149c8cea77efdc1
Oliver <ylin73505@gmail.com>
2026-06-07 20:34:46 +0800

    設定產品編號為輸入型屬性

--- src/app/product-card/product-card.component.ts
+++ src/app/product-card/product-card.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, numberAttribute } from '@angular/core';
 
 @Component({
   selector: 'app-product-card',
@@ -7,6 +7,9 @@ import { Component, Input } from '@angular/core';
   styleUrl: './product-card.component.scss',
 })
 export class ProductCardComponent {
+  @Input({ required: true, transform: numberAttribute })
+  id!: number;
+
   @Input()
   productName!: string;
 
