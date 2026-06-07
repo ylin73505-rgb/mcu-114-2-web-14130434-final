cde901edc9736469dcfc5a9bc0e4b957c7758591
Oliver <ylin73505@gmail.com>
2026-06-07 20:02:55 +0800

    利用文字插值顯示產品名稱、作者與公司資訊

--- src/app/product-card/product-card.component.ts
+++ src/app/product-card/product-card.component.ts
@@ -6,4 +6,8 @@ import { Component } from '@angular/core';
   templateUrl: './product-card.component.html',
   styleUrl: './product-card.component.scss',
 })
-export class ProductCardComponent {}
+export class ProductCardComponent {
+  protected productName = 'A產品';
+  protected author = '作者甲、作者乙、作者丙';
+  protected company = '博碩文化';
+}
