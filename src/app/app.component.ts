95f65997f368ec11b6cc8ec2454d183a7e1ab818
Oliver <ylin73505@gmail.com>
2026-06-07 19:41:41 +0800

    建立 angular 專案

---
+++ src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component, signal } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss',
+})
+export class App {
+  protected readonly title = signal('shopping-cart');
+}
