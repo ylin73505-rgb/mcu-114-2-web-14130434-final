4555d4eca4610f45675a23552196ca331618bc55
Oliver <ylin73505@gmail.com>
2026-06-10 21:32:28 +0800

    新增標題元件實作

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
+
 import { ProductCardListComponent } from './product-card-list/product-card-list.component';
 import { Product } from './model/product';
+import { HeaderComponent } from './header/header.component';
 
 @Component({
   selector: 'app-root',
-  imports: [ProductCardListComponent],
+  imports: [HeaderComponent, ProductCardListComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
