a32325517dde39111c82180f6477955ab4b35a1f
Oliver <ylin73505@gmail.com>
2026-06-10 21:13:51 +0800

    依產品清單顯示實作

--- src/app/product-card-list/product-card-list.component.ts
+++ src/app/product-card-list/product-card-list.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 import { ProductCardComponent } from '../product-card/product-card.component';
 import { Product } from '../model/product';
@@ -10,13 +10,6 @@ import { Product } from '../model/product';
   styleUrl: './product-card-list.component.scss',
 })
 export class ProductCardListComponent {
-  product = new Product({
-    id: 1,
-    name: '書籍 A',
-    authors: '作者甲、作者乙、作者丙',
-    company: '博碩文化',
-    photoUrl: 'https://api.fnkr.net/testimg/200x200/DDDDDD/999999/?text=img',
-    createDate: new Date('2025/4/9'),
-    price: 10000,
-  });
+  @Input()
+  products!: Product[];
 }
