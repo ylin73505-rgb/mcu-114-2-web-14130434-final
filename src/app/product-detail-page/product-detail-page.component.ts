08c6bfb5fe9cd531637edbcaff716393a193faa8
Oliver <ylin73505@gmail.com>
2026-06-15 14:45:50 +0800

    實作依產品編號取得明細資料

--- src/app/product-detail-page/product-detail-page.component.ts
+++ src/app/product-detail-page/product-detail-page.component.ts
@@ -1,7 +1,8 @@
 import { CurrencyPipe } from '@angular/common';
-import { Component, inject, signal } from '@angular/core';
-import { Product } from '../model/product';
+import { Component, OnInit, inject, input, numberAttribute, signal } from '@angular/core';
 import { Router } from '@angular/router';
+import { Product } from '../model/product';
+import { ProductService } from '../services/product.service';
 
 @Component({
   selector: 'app-product-detail-page',
@@ -9,20 +10,19 @@ import { Router } from '@angular/router';
   templateUrl: './product-detail-page.component.html',
   styleUrl: './product-detail-page.component.scss',
 })
-export class ProductDetailPageComponent {
-  protected readonly product = signal(
-    new Product({
-      id: 9,
-      name: '書籍 I',
-      authors: '作者甲、作者乙、作者丙',
-      company: '博碩文化',
-      photoUrl: 'https://api.fnkr.net/testimg/200x200/DDDDDD/999999/?text=img',
-      createDate: new Date('2025/4/9'),
-      price: 10000,
-    })
-  );
+export class ProductDetailPageComponent implements OnInit {
+  id = input.required<number, string | number>({ transform: numberAttribute });
+
+  protected readonly product = signal<Product>(new Product());
 
   private readonly router = inject(Router);
+
+  private readonly productService = inject(ProductService);
+
+  ngOnInit(): void {
+    const product = this.productService.getById(this.id());
+    this.product.set(product);
+  }
 
   onEdit(): void {
     this.router.navigate(['product', 'form', this.product().id]);
--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -104,4 +104,8 @@ export class ProductService {
     const data = name ? this._data.filter((item) => item.name === name) : [...this._data];
     return { data: data.slice(startIndex, endIndex), count: this._data.length };
   }
+
+  getById(productId: number): Product {
+    return this._data.find(({ id }) => id === productId)!;
+  }
 }
