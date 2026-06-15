bc1f113c1779c9027959d24d7e19fb8e4d77afca
Oliver <ylin73505@gmail.com>
2026-06-15 14:10:31 +0800

    從服務取得產品資料

--- src/app/product-page/product-page.component.ts
+++ src/app/product-page/product-page.component.ts
@@ -1,8 +1,9 @@
-import { Component, inject } from '@angular/core';
+import { Component, OnInit, inject, signal } from '@angular/core';
 import { Router } from '@angular/router';
 
 import { ProductCardListComponent } from '../product-card-list/product-card-list.component';
 import { Product } from '../model/product';
+import { ProductService } from '../services/product.service';
 
 @Component({
   selector: 'app-product-page',
@@ -10,57 +11,19 @@ import { Product } from '../model/product';
   templateUrl: './product-page.component.html',
   styleUrl: './product-page.component.scss',
 })
-export class ProductPageComponent {
-  private router = inject(Router);
-  protected products: Product[] = [
-    new Product({
-      id: 1,
-      name: '書籍 A',
-      authors: '作者甲、作者乙、作者丙',
-      company: '博碩文化',
-      photoUrl: 'https://api.fnkr.net/testimg/200x200/DDDDDD/999999/?text=img',
-      createDate: new Date('2025/4/9'),
-      price: 1580,
-    }),
-    new Product({
-      id: 2,
-      name: '書籍 B',
-      authors: '作者甲、作者乙、作者丙',
-      company: '博碩文化',
-      photoUrl: 'https://api.fnkr.net/testimg/200x200/DDDDDD/999999/?text=img',
-      createDate: new Date('2025/4/9'),
-      price: 10000,
-    }),
-    new Product({
-      id: 3,
-      name: '書籍 C',
-      authors: '作者甲、作者乙、作者丙',
-      company: '博碩文化',
-      photoUrl: 'https://api.fnkr.net/testimg/200x200/DDDDDD/999999/?text=img',
-      createDate: new Date('2025/4/9'),
-      price: 1580,
-    }),
-    new Product({
-      id: 4,
-      name: '書籍 D',
-      authors: '作者甲、作者乙、作者丙',
-      company: '博碩文化',
-      photoUrl: 'https://api.fnkr.net/testimg/200x200/DDDDDD/999999/?text=img',
-      createDate: new Date('2025/4/9'),
-      price: 1580,
-    }),
-    new Product({
-      id: 5,
-      name: '書籍 E',
-      authors: '作者甲、作者乙、作者丙',
-      company: '博碩文化',
-      photoUrl: 'https://api.fnkr.net/testimg/200x200/DDDDDD/999999/?text=img',
-      createDate: new Date('2025/4/9'),
-      price: 1580,
-    }),
-  ];
+export class ProductPageComponent implements OnInit {
+  private readonly router = inject(Router);
+
+  private productService!: ProductService;
+
+  protected readonly products = signal<Product[]>([]);
+
+  ngOnInit(): void {
+    this.productService = new ProductService();
+    this.products.set(this.productService.getList());
+  }
 
   protected onView(product: Product): void {
-    this.router.navigate(['product', product.id]);
+    this.router.navigate(['product', 'view', product.id]);
   }
 }
